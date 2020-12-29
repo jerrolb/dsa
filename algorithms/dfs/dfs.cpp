ab24d61ba34d4cf1404fa576399a7df59f94b596
jerrolb <jerrol.brooks@nagrastar.com>
2020-12-29 15:00:25 -0700

    dfs

--- algorithms/dfs/dfs.cpp
+++ algorithms/dfs/dfs.cpp
@@ -1,84 +1,70 @@
-#include<bits/stdc++.h> 
-using namespace std; 
-  
-// Graph class represents a directed graph 
-// using adjacency list representation 
-class Graph 
-{ 
-    int V;    // No. of vertices 
-  
-    // Pointer to an array containing 
-    // adjacency lists 
-    list<int> *adj; 
-  
-    // A recursive function used by DFS 
-    void DFSUtil(int v, bool visited[]); 
-public: 
-    Graph(int V);   // Constructor 
-  
-    // function to add an edge to graph 
-    void addEdge(int v, int w); 
-  
-    // DFS traversal of the vertices 
-    // reachable from v 
-    void DFS(int v); 
-}; 
-  
-Graph::Graph(int V) 
-{ 
-    this->V = V; 
-    adj = new list<int>[V]; 
-} 
-  
-void Graph::addEdge(int v, int w) 
-{ 
-    adj[v].push_back(w); // Add w to v’s list. 
-} 
-  
-void Graph::DFSUtil(int v, bool visited[]) 
-{ 
-    // Mark the current node as visited and 
-    // print it 
-    visited[v] = true; 
-    cout << v << " "; 
-  
-    // Recur for all the vertices adjacent 
-    // to this vertex 
-    list<int>::iterator i; 
-    for (i = adj[v].begin(); i != adj[v].end(); ++i) 
-        if (!visited[*i]) 
-            DFSUtil(*i, visited); 
-} 
-  
-// DFS traversal of the vertices reachable from v. 
-// It uses recursive DFSUtil() 
-void Graph::DFS(int v) 
-{ 
-    // Mark all the vertices as not visited 
-    bool *visited = new bool[V]; 
-    for (int i = 0; i < V; i++) 
-        visited[i] = false; 
-  
-    // Call the recursive helper function 
-    // to print DFS traversal 
-    DFSUtil(v, visited); 
-} 
-  
-// Driver code 
-int main() 
-{ 
-    // Create a graph given in the above diagram 
-    Graph g(4); 
-    g.addEdge(0, 1); 
-    g.addEdge(0, 2); 
-    g.addEdge(1, 2); 
-    g.addEdge(2, 0); 
-    g.addEdge(2, 3); 
-    g.addEdge(3, 3); 
-  
-    cout << "Following is Depth First Traversal"
-            " (starting from vertex 2) \n"; 
-    g.DFS(2); // 2 0 1 3
-  
-    return 0; 
-} 
+#include<bits/stdc++.h>
+using namespace std;
+
+class Graph
+{
+    int V;
+    list<int> *adj;
+    void dfs(int v, bool visited[]);
+
+    public:
+        Graph(int V);
+        void addEdge(int v, int w);
+        void init_dfs(int v);
+};
+
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w)
+{
+    adj[v].push_back(w);
+}
+
+void Graph::dfs(int v, bool visited[])
+{
+    visited[v] = true;
+    cout << v << " ";
+
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            dfs(*i, visited);
+}
+
+void Graph::init_dfs(int v)
+{
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+
+    dfs(v, visited);
+}
+
+int main()
+{
+    int num_of_vertices = 5;
+    Graph g(num_of_vertices);
+
+    // Add both directions to simulate undirected graph
+    g.addEdge(0, 1);
+    g.addEdge(1, 0);
+    g.addEdge(0, 2);
+    g.addEdge(2, 0);
+    g.addEdge(1, 3);
+    g.addEdge(3, 1);
+    g.addEdge(1, 4);
+    g.addEdge(4, 1);
+
+    for (int start_vertex = 0; start_vertex < num_of_vertices; ++start_vertex)
+    {
+        cout << "DFS starting from vertex " << start_vertex << ": ";
+        g.init_dfs(start_vertex);
+        cout << endl;
+    }
+
+    return 0;
+}
